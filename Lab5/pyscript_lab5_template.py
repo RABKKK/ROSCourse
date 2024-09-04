19872d8c850b8e4ed08ed228b9f3b9c51b382e74
Riby Abraham Boby <55803102+RABKKK@users.noreply.github.com>
2024-09-04 10:48:53 +0530

    Add files via upload

---
+++ Lab5/pyscript_lab5_template.py
@@ -0,0 +1,31 @@
+from sensor_read_class import ReadSensor
+from matplotlib import pyplot as plt
+#Additional libraries that may be used (try not use so many libraries) only what is required to ensure the program has minimum requirements
+
+#Refer to previous lab tasks to understand how Methods are called. Also check the class defined in sensor_read_class.py file
+  
+
+#maximum no of terations plotting should happen
+max_iter=10
+
+#Minimum and maximum distances to define workspace
+#xyrange=[0, 1, -1, 1]
+
+#define object
+
+#define figure for plotting
+fig = plt.figure()
+
+#initialize iterat
+
+while (iterat<max_iter):
+  #Obtain the data to be plotted after filtering within ranges
+  #Plot filtered values using  plt.plot(<xvalues>,<yvalues>,color=<color>,ls='None',marker=<marker>)
+  #plot mean using plt.plot(<xvalues>,<yvalues>,color=<color>,marker=<marker>)
+  #Set x and y limits plt.xlim(<xlimitmin>,<xlimitmax>) using xyrange
+  #Set x and y limits plt.ylim(<ylimitmin>,<ylimitmax>) using xyrange
+  #set label for axis plt.xlabel(<xlabel>)
+  #set label for axis plt.ylabel(<ylabel>)    
+plt.close('all')  
+
+ 
